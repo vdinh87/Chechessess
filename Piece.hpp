9e658fab7fb2c7d4304e523743e889a0b3d6a3c1
--replace-all <kwander31@gmail.com>
2023-06-15 01:58:08 -0400

    Implemented Piece Abstract Data Type with Inheritance Hierarchy
    a centralized container for common piece attributes and behavior. basis for polymorphism and facilitates extensibility by allowing derived classes to specialize their functionality. Piece -> SpecificPiece -> SpecificSuperPiece.

--- Piece.hpp
+++ Piece.hpp
@@ -1,2 +1,41 @@
 #pragma once
+#include <list>
+#include <string>
+
+class Piece {
+
+private:
+ bool color_; //0 black   |    1 white
+ bool is_superpiece;
+ std::list<std::string> movesList;
+ std::string location;
+ int tier;
+ std::string type_;
+
+public:
+ Piece(std::string type, bool color): type_(type), color_(color) {}
+ virtual ~Piece() = default;
+
+ //getters
+ bool getColor() const  {
+  return color_;
+ }
+ std::string getType() const {
+  return type_;
+ }
+ bool isSuperPiece() const {
+  return is_superpiece;
+ }
+ std::list<std::string> getmovesList() const {
+  return movesList;
+ }
+ std::string getLocation() const {
+  return location;
+ }
+ int getTier() const {
+  return tier;
+ }
+
+
+};
 
