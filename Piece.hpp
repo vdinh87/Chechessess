d7cd9d054036562e3b250be8314f297037878889
--replace-all <kwander31@gmail.com>
2023-06-17 00:46:44 -0400

    wadwa

--- Piece.hpp
+++ Piece.hpp
@@ -1,41 +1,34 @@
 #pragma once
 #include <list>
 #include <string>
+#include <unordered_map>
 
-class Piece {
-
+class Piece
+{
 private:
- bool color_; //0 black   |    1 white
+ bool color_; // 0 black | 1 white
  bool is_superpiece;
- std::list<std::string> movesList;
  std::string location;
  int tier;
  std::string type_;
+ static std::unordered_map<std::string, std::list<std::string>> movelist_;
 
 public:
- Piece(std::string type, bool color): type_(type), color_(color) {}
+ Piece(std::string type, bool color) : type_(type), color_(color), is_superpiece(false)  {
+  if (movelist_.empty())
+   movelist_ = std::unordered_map<std::string, std::list<std::string>>();
+ }
+
  virtual ~Piece() = default;
 
- //getters
- bool getColor() const  {
-  return color_;
- }
- std::string getType() const {
-  return type_;
- }
- bool isSuperPiece() const {
-  return is_superpiece;
- }
- std::list<std::string> getmovesList() const {
-  return movesList;
- }
- std::string getLocation() const {
-  return location;
- }
- int getTier() const {
-  return tier;
- }
-
-
+ // Getters
+ bool getColor() const { return color_; }
+ std::string getType() const { return type_; }
+ bool isSuperPiece() const { return is_superpiece; }
+ std::string getLocation() const { return location; }
+ int getTier() const { return tier; }
+ static std::unordered_map<std::string, std::list<std::string>> &getMoveList() { return movelist_; }
 };
 
+// Initialize the static member variable outside the class definition
+std::unordered_map<std::string, std::list<std::string>> Piece::movelist_;
