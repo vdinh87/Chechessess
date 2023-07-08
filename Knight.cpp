cdc9409ab49428d84a8ab5fc72e3c1d86b59aa5f
meromeofbacca <vdinh2020@gmail.com>
2023-07-07 23:14:22 -0400

    minor change

--- Knight.cpp
+++ Knight.cpp
@@ -15,7 +15,7 @@ U64 Knight::GetAttacks(U64 occupancy_) const
     U64 attacks, knights = 0ULL;
     set_bit( knights, GetPosition() ); 
     attacks = ( (knights & ( FILE_G | FILE_H)) ? 0ULL: (knights >> 6) | (knights << 10)) ;
-    // attacks = (((knights >> 6) | (knights << 10)) & ~(knights & FILE_G | knights & FILE_H)) |
+    // attacks = (((knights >> 6) | (knights << 10)) & ~(FILE_G | FILE_H)) |
     //     (((knights >> 10) | (knights << 6)) & ~(FILE_A | FILE_B)) |
     //     (((knights >> 15) | (knights << 17)) & ~FILE_H) |
     //     (((knights >> 17) | (knights << 15)) & ~FILE_A);
