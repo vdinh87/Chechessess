e7e46b6cbd13d29aecbd73cfa1f0783f79e77eaf
meromeofbacca <vdinh2020@gmail.com>
2023-09-23 01:28:55 -0400

    fixed castle not showing up in logger

--- Logger.cpp
+++ Logger.cpp
@@ -16,42 +16,44 @@ void Logger::PrintLog()
     {
         std::string line;
         ChessMove move = log[i];
-    
-        //add piece type
-        line += PieceStrings[move.type];
 
-        //capture
-        auto it = std::find(move.actions.begin(), move.actions.end(), Action::Capture);
-        if(it != move.actions.end()) //
-            line += "x";
-
-        //destination square
-        if( move.to != Square::invalid )
-            line += SquareStrings[move.to];
-        
-        //promotion
-        it = std::find(move.actions.begin(), move.actions.end(), Action::Promotion);
-        if(it != move.actions.end()) //
-            line += "=" + PieceStrings[move.type];
-        
         //castling
-        it = std::find(move.actions.begin(), move.actions.end(), Action::Castle);
-        if(it != move.actions.end()) //
+        auto it = std::find(move.actions.begin(), move.actions.end(), Action::Castle);
+        if(it != move.actions.end()) 
             line += "O-O";
 
-        //checkmate
-        it = std::find(move.actions.begin(), move.actions.end(), Action::Checkmate);
-        if(it != move.actions.end()) //
-            line += "#";
-
-        //check
-        if( it == move.actions.end() ) //if not checkmate proceed
+        if( it == move.actions.end() ) //if not castle proceed
         {
-            it = std::find(move.actions.begin(), move.actions.end(), Action::Check);
-            if(it != move.actions.end()) //
-                line += "+";
+            //add piece type
+            line += PieceStrings[move.type];
+
+            //capture
+            it = std::find(move.actions.begin(), move.actions.end(), Action::Capture);
+            if(it != move.actions.end()) 
+                line += "x";
+
+            //destination square
+            if( move.to != Square::invalid )
+                line += SquareStrings[move.to];
+            
+            //promotion
+            it = std::find(move.actions.begin(), move.actions.end(), Action::Promotion);
+            if(it != move.actions.end()) 
+                line += "=" + PieceStrings[move.type];
+            
+            //checkmate
+            it = std::find(move.actions.begin(), move.actions.end(), Action::Checkmate);
+            if(it != move.actions.end()) 
+                line += "#";
+
+            //check
+            if( it == move.actions.end() ) //if not checkmate proceed
+            {
+                it = std::find(move.actions.begin(), move.actions.end(), Action::Check);
+                if(it != move.actions.end()) 
+                    line += "+";
+            }
         }
-        
         int width = 8;
         std::cout << "| " << std::setw(width) << std::left << line ;
         if( i % 2 == 1) //black move
