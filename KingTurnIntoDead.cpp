0acb0b4cd66f964a1febf22c36d9b13df3e0e761
meromeofbacca <vdinh2020@gmail.com>
2023-10-15 13:28:51 -0400

    finished KingTurnIntoDead ability

--- KingTurnIntoDead.cpp
+++ KingTurnIntoDead.cpp
@@ -2,7 +2,7 @@
 #include "KingTurnIntoDead.hpp"
 
 KingTurnIntoDead::KingTurnIntoDead(SuperChessGame* game_) :
-Ability("King Sniper Shot", game_)
+Ability("King Turn Into Dead", game_)
 {
 }
 
@@ -24,6 +24,10 @@ void KingTurnIntoDead::Effect(const SuperPiece& piece)
 
     int current_turn = (log_.size() + 1) / 2;
 
+    if( sq == piece.GetSquare() ){
+        std::cout << "Cannot choose yourself\n";
+        return;
+    }
     if (piece.GetColor() != game->GetColor(1ULL << sq)) { 
         std::cout << "Invalid square different color piece." << std::endl;
         return;
@@ -64,6 +68,17 @@ void KingTurnIntoDead::Effect(const SuperPiece& piece)
     SuperPieceInfo key;
     if( selection < graveyard.size() && selection >= 0 ) //valid number, not super strict checking
         key = graveyard[selection];    
+    else{
+        std::cout << "Not valid selection\n";
+        return;
+    }
+    game->RemovePiece(sq);
+    if( key.second != Tier::not_superpiece ) //if dead piece was superpiece
+        game->AddSuperPiece(key, sq, piece.GetColor());
+    else
+        game->AddPiece(sq, piece.GetColor(), key.first);
+
+    std::cout << "King Turn Into Dead successful";
 }
 
 std::unique_ptr<Ability> KingTurnIntoDead::Clone() const
