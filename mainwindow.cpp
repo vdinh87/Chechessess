7a9121b6e8ab84f62f006b4748f5f5118024e408
meromeofbacca <vdinh2020@gmail.com>
2024-01-16 21:34:53 -0500

    update to previous

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -5,9 +5,91 @@
 #include "ChessEngine/ChessGame.cpp"
 #include "ChessEngine/Logger.cpp"
 
-// ChessGame cg;
+ChessGame cg;
 void updateLabelsFromBitboard(uint64_t bitboard, std::vector<DraggableLabel*>& draggableLabels);
 
+void PlayChessGame()
+{
+    std::unique_ptr<ChessGame> cg = std::make_unique<ChessGame>();
+
+    while( !cg->IsWin(white) && !cg->IsWin(black) )
+    {
+        ChessMove move;
+
+        for( int i = white; i <= black && ( !cg->IsWin(white) && !cg->IsWin(black) ); i++)
+        {
+            Color color = static_cast<Color>(i);
+            std::string input_str;
+            Square move_to_square = invalid;
+            U64 move_list = 0;
+            Square piece = invalid;
+            while( move_to_square == invalid )
+            {
+                Square selected_square = invalid;
+                U64 square = 0;
+                // Invalid selected square loop
+                while( !(cg->GetPiecesOfColor(color) & square) )
+                {
+                    cg->PrintBoard();
+                    std::cout << ColorStrings[color] << " to move." << std::endl;
+                    std::cout << "Select valid square: " ;
+
+                    // Input to square
+                    std::cin >> input_str;
+                    std::cout << std::endl;
+
+                    auto it = SqStrMap.find(input_str);
+                    if (it != SqStrMap.end())
+                        selected_square = it->second;
+                    else
+                        std::cout << "Invalid square" << std::endl;
+
+                    //Selected square is in movelist
+                    set_bit(square, selected_square);
+
+                    if( square & move_list )
+                    {
+                        move_to_square = GetSquare(square);
+                        break;
+                    }
+                    else
+                        move_list = 0;
+                    //Set piece
+                    piece = selected_square;
+                }
+
+                if( move_to_square == invalid)
+                {
+                    move_list = cg->GetAttacks(selected_square);
+                    std::cout << "Movelist of piece at square: " << input_str << std::endl;
+                    PrintGoard(move_list);
+                }
+            }
+
+            //move piece
+            move.type = cg->GetPieceType(1ULL << piece);
+            move.from = piece;
+            move.to = move_to_square;
+            std::vector<Action> action_list = cg->Move( piece, move_to_square);
+            if( cg->IsWin(white) || cg->IsWin(black) )
+            {
+                action_list.push_back(Action::Checkmate);
+                move.actions = action_list;
+                cg->Log(move);
+                break;
+            }
+
+            move.actions = action_list;
+            cg->Log(move);
+        }
+    }
+
+    if( cg->IsWin(white) )
+        std::cout << "White win!" << std::endl;
+    else
+        std::cout << "Black win!" << std::endl;
+    cg->PrintTheLog();
+}
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
@@ -42,7 +124,7 @@ MainWindow::MainWindow(QWidget *parent)
 
     //labels
     // ui->label_54->setLabels()
-
+    PlayChessGame();
 }
 
 
