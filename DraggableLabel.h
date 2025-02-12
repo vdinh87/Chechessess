4dc864062d964380bbe685c67121ce1fe5477909
Kevin Wander <87961130+Kwander@users.noreply.github.com>
2025-02-11 23:19:42 -0500

    Fix dragging into itself deleting it
    Clicking a piece without moving the mouse won't make it disappear
    You need to actually drag the piece a small distance before the drag operation starts
    The piece will only disappear from its original position if it was successfully dropped somewhere else

--- DraggableLabel.h
+++ DraggableLabel.h
@@ -5,26 +5,29 @@
 #include <QDrag>
 #include <QMimeData>
 #include <QMouseEvent>
+#include <QApplication>
 
-
-class DraggableLabel : public QLabel {
+class DraggableLabel : public QLabel
+{
     Q_OBJECT
 public:
     DraggableLabel();
-    DraggableLabel(QWidget *parent = 0) : QLabel(parent) {
+    DraggableLabel(QWidget *parent = 0) : QLabel(parent)
+    {
         setAcceptDrops(true);
     }
     void setDraggable(bool can_drag)
     {
         draggable = can_drag;
     }
-    void updateLabelsFromBitboard(uint64_t bitboard, std::vector<DraggableLabel*>& draggableLabels){
-        for (int i = 0; i < 64; i++) {
+    void updateLabelsFromBitboard(uint64_t bitboard, std::vector<DraggableLabel *> &draggableLabels)
+    {
+        for (int i = 0; i < 64; i++)
+        {
             DraggableLabel *label = draggableLabels[i];
-
             bool bit = (bitboard & (1ULL << i));
-
-            if (bit) {
+            if (bit)
+            {
                 label->setStyleSheet("border: 1px solid black;");
             }
         }
@@ -32,54 +35,67 @@ public:
 
 protected:
     bool draggable = false;
+    QPoint dragStartPosition;
 
-    void mousePressEvent(QMouseEvent *event) override {
-        if (!draggable) return;
-
-        if (event->button() == Qt::LeftButton) {
-            QDrag *Drag = new QDrag(this);
-            QMimeData *mimeData = new QMimeData;
-
-
-            // Here we could add any data we want to the mime data object
-            mimeData->setText(this->styleSheet());
-            Drag->setMimeData(mimeData);
-
-            QPixmap pixmap(this->size());
-            this->render(&pixmap);
-            Drag->setPixmap(pixmap);
-
-            Drag->exec();
-
-            if (Drag->target() != this) {
-                this->setStyleSheet("    border-image: url(:/img/blank.png) 0 0 0 0 stretch stretch;\\n");
-                this->draggable = false;
-
-            }
+    void mousePressEvent(QMouseEvent *event) override
+    {
+        if (!draggable)
+            return;
+        if (event->button() == Qt::LeftButton)
+        {
+            dragStartPosition = event->pos();
         }
-
-
     }
 
-    void dragEnterEvent(QDragEnterEvent *event) override {
-        if (event->mimeData()->hasText()) {
+    void mouseMoveEvent(QMouseEvent *event) override
+    {
+        if (!draggable)
+            return;
+        if (!(event->buttons() & Qt::LeftButton))
+            return;
+
+        if ((event->pos() - dragStartPosition).manhattanLength() < QApplication::startDragDistance())
+            return;
+
+        QDrag *drag = new QDrag(this);
+        QMimeData *mimeData = new QMimeData;
+
+        // Store the original style and position
+        mimeData->setText(this->styleSheet());
+        drag->setMimeData(mimeData);
+
+        QPixmap pixmap(this->size());
+        this->render(&pixmap);
+        drag->setPixmap(pixmap);
+
+        Qt::DropAction dropAction = drag->exec();
+
+        if (dropAction != Qt::IgnoreAction && drag->target() != this)
+        {
+            this->setStyleSheet("border-image: url(:/img/blank.png) 0 0 0 0 stretch stretch;\\n");
+            this->draggable = false;
+        }
+    }
+
+    void dragEnterEvent(QDragEnterEvent *event) override
+    {
+        if (event->mimeData()->hasText())
+        {
             emit dragEntered(this->objectName());
             event->acceptProposedAction();
         }
     }
 
-    void dropEvent(QDropEvent *event) override {
-        if (event->source() != this) {
+    void dropEvent(QDropEvent *event) override
+    {
+        if (event->source() != this)
+        {
             this->setStyleSheet(event->mimeData()->text());
         }
     }
 
-
-
 signals:
     void dragEntered(QString objectName);
-
 };
-
 
 #endif // DraggableLabel_H
